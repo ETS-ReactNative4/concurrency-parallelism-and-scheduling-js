b2b49aaf570999741a5913d1492df92046c2dc47
Mihail Mikov <m.mikoff@gmail.com>
2018-09-15 00:01:57 +0300

    stopped web worker..

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<Presentation />, document.getElementById('root'));
 registerServiceWorker();
 
-const worker = new Worker('/static/js/worker.js');
-
-worker.onmessage = (message) => {
-  console.log(message);
-  setTimeout(() => {
-    worker.postMessage('ping');
-  }, 2000);
-};
-
-worker.postMessage('ping');
+// const worker = new Worker('/static/js/worker.js');
+//
+// worker.onmessage = (message) => {
+//   console.log(message);
+//   setTimeout(() => {
+//     worker.postMessage('ping');
+//   }, 2000);
+// };
+//
+// worker.postMessage('ping');
